1183f0c2e01af9256a68cd2a20d254a4d570ec6a
Eduardo Querido <eduardo.rumbels@gmail.com>
2020-10-10 19:26:38 -0300

    Implementado Estado Verde TDD com utilização da técnica de Partições de Equivalência
    - Implementado o método JuroSimples da classe CalculoJuroSimples
    
    	- Verificação de valores superiores e inferiores ao intervalo de tempo(periodo);
    		- Retorno 0, caso se enquaddre nessas duas situalções
    	- Retorno do cálculo

--- entregas_testes_funcionais/atividade_pratica_01/src/main/java/atividade_pratica_01/CalculoJuroSimples.java
+++ entregas_testes_funcionais/atividade_pratica_01/src/main/java/atividade_pratica_01/CalculoJuroSimples.java
@@ -4,6 +4,17 @@ public class CalculoJuroSimples {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+	}
 
+	public static float JuroSimples(float capital, float taxa, float periodo) {
+
+			if (periodo > 12f) {
+				return 0f;
+			} else if (periodo < 0f){
+				return 0f;
+			}
+			else{
+				return capital * taxa * periodo;
+		}
 	}
 }
