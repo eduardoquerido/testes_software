3e2df924ad79d62065cd9a9c993fcc6d7153c699
Eduardo Querido <eduardo.rumbels@gmail.com>
2020-10-10 19:30:37 -0300

    Implementado o estado AZUL, refatoramento do código
    - Melhorado as verificações do código
    
    - Adicionado verificação caso taxa e capital também sejam menores que 0, visto
    que não existe cálculo para taxa e/ou capital negativos, juntamente com periodo negativo
    
    - Retirado os 2 ifs e utilziado somente um, verificando todas as condições, tais qual:
    
    	- se o periodo for menos do que 0 ou maior do que 12 meses;
    	- adiciona a verificação caso a taxa e/ou o capital forem 0;

--- entregas_testes_funcionais/atividade_pratica_01/src/main/java/atividade_pratica_01/CalculoJuroSimples.java
+++ entregas_testes_funcionais/atividade_pratica_01/src/main/java/atividade_pratica_01/CalculoJuroSimples.java
@@ -8,9 +8,7 @@ public class CalculoJuroSimples {
 
 	public static float JuroSimples(float capital, float taxa, float periodo) {
 
-			if (periodo > 12f) {
-				return 0f;
-			} else if (periodo < 0f){
+			if (periodo < 0f || periodo > 12 || capital < 0f || taxa < 0f) {
 				return 0f;
 			}
 			else{
