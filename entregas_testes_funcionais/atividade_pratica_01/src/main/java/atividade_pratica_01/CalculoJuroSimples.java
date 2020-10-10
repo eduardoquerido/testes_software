3949c00be44c178e672752ef3b181ef3803e263c
Eduardo Querido <eduardo.rumbels@gmail.com>
2020-10-10 17:26:58 -0300

    Implementando Estado Vermelho TDD com utilização da técnica de Partições de Equivalência
    - Escolhido um intervalo para periodo de [1, 12] meses
    
    - Implementados 3 TestCases
    
    	- TestCase 1 - ([1600, 0.005, 4, JuroSimples]320)
    
    	- TestCase 2 - ([3200, 0.10, -6, JuroSimples]0)
    
    	- TestCase 3 - ([4400, 0.5, 15, JuroSimples]0)

--- entregas_testes_funcionais/atividade_pratica_01/src/main/java/atividade_pratica_01/CalculoJuroSimples.java
+++ entregas_testes_funcionais/atividade_pratica_01/src/main/java/atividade_pratica_01/CalculoJuroSimples.java
@@ -2,4 +2,8 @@ package atividade_pratica_01;
 
 public class CalculoJuroSimples {
 
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
 }
--- entregas_testes_funcionais/atividade_pratica_01/src/test/java/atividade_pratica_01/CalculoJuroSimplesTest.java
+++ entregas_testes_funcionais/atividade_pratica_01/src/test/java/atividade_pratica_01/CalculoJuroSimplesTest.java
@@ -6,4 +6,76 @@ import static org.junit.jupiter.api.Assertions.assertEquals;;
 // Intervalo de Tempo entre 0 a 12 meses
 class CalculoJuroSimplesTest {
 
+	@Test
+	public void testJuroSimplesParticaoA() {
+      // Partição A - Teste Válido no intervalo [0, 12]
+		
+      //TestCase 1 - ([1600, 0.005, 4, JuroSimples]320)
+      //Capital Inicial: R$ 1.600,00
+      //Taxa de Juros: 0.07/mês
+      //Tempo aplicado: 4 meses
+	
+      // Atribuição de valores para o Teste Case 1
+      float capital = 1000f;
+      float taxa = 0.07f;
+      float periodo = 4f; // tempo determinado em MESES
+      
+      //Resultado Esperado do TestCase 1
+      float resultado_esperado = 280f;
+      //Retorno do método de JuroSimples para TC1
+      float resultado_obtido = CalculoJuroSimples.JuroSimples(capital, taxa, periodo);
+      // assertEquals(double expected, double actual, double delta)
+      assertEquals(resultado_esperado, resultado_obtido);
+}
+	
+	
+	@Test
+	public void testJuroSimplesParticaoB() {
+		// Partição B - Teste Inválido no intervalo ]-1, -inf]
+		
+		//TestCase 2 - ([3200, 0.10, -6, JuroSimples]0)
+	    //Capital Inicial: R$ 3.200,00
+	    //Taxa de Juros: 0.10/mês
+        //Tempo aplicado: -6 meses
+		
+	      // Atribuição de valores para o Teste Case 1
+	      float capital = 3200f;
+	      float taxa = 0.1f;
+	      float periodo = -4f; // tempo determinado em MESES
+	      
+	      //Resultado Esperado do TestCase 1
+	      float resultado_esperado = 0f;
+
+	      //Retorno do método de JuroSimples para TC1
+	      float resultado_obtido = CalculoJuroSimples.JuroSimples(capital, taxa, periodo);
+
+	      // assertEquals(double expected, double actual, double delta)
+	      assertEquals(resultado_esperado, resultado_obtido);
+	}
+	
+	
+	@Test
+	public void testJuroSimplesParticaoC() {
+		// Partição C - Teste Inválido no intervalo [13, +inf[
+		
+		//TestCase 3 - ([4400, 0.5, 15, JuroSimples]0)
+	    //Capital Inicial: R$ 3.200,00
+	    //Taxa de Juros: 0.05/mês
+        //Tempo aplicado: 15 meses
+		
+	      // Atribuição de valores para o Teste Case 1
+	      float capital = 4400f;
+	      float taxa = 0.5f;
+	      float periodo = 15f; // tempo determinado em MESES
+	      
+	      //Resultado Esperado do TestCase 1
+	      float resultado_esperado = 0f;
+
+	      //Retorno do método de JuroSimples para TC1
+	      float resultado_obtido = CalculoJuroSimples.JuroSimples(capital, taxa, periodo);
+
+	      // assertEquals(double expected, double actual, double delta)
+	      assertEquals(resultado_esperado, resultado_obtido);
+	}
+
 }
